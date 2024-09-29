fa3eff9729ed0a050f0fd4780b0076d3e239b702
brunosr3003 <70835031+brunosr3003@users.noreply.github.com>
2024-09-29 00:24:36 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',  // Caminho base para sites na raiz
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-      }
-    }
-  }
 });
