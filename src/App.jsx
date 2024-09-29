39008e12e5bc76cc2cf4708f1ffd1bf792951ec6
brunosr3003 <70835031+brunosr3003@users.noreply.github.com>
2024-09-28 23:49:32 -0300

    asda

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";  // Certifique-se de que o caminho está correto
 import HomePage from "./pages/HomePage";
-import SegundaPagina from "./pages/SegundaPagina";
+import SegundaPagina from "./pages/segundaPagina";
 
 const App = () => {
   return (
