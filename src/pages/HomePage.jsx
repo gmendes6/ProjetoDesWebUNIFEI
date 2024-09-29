d04c31b15db6778721e5d41f45491b916baf1f5d
brunosr3003 <70835031+brunosr3003@users.noreply.github.com>
2024-09-29 00:56:05 -0300

    .

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,17 +6,29 @@ import Testimonials from "../components/Testimonials/Testimonials";
 import Footer from "../components/Footer/Footer";
 import Banner2 from "../components/Banner/Banner2"
 
+import React, { useEffect, useState } from "react"
 
-const App = () => {
+const HomePage = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Faz a chamada à API do backend na Vercel
+    fetch('/api/ssr')
+      .then(response => response.json())
+      .then(data => setData(data.message))
+      .catch(error => console.error('Error fetching the API:', error));
+  }, []);
+
   return (
-    <div className="overflow-x-hidden">
-      <Navbar />
+    <div>
+     <Navbar />
       <Equipments />
       <TabComp />
       <Testimonials />
       <Banner2/>
+      <p>{data ? data : "Loading data from API..."}</p>
     </div>
   );
 };
 
-export default App;
+export default HomePage;
