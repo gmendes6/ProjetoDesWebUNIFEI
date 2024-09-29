35aa013ae3a00669bb8695676b431f300ff8b8f3
brunosr3003 <70835031+brunosr3003@users.noreply.github.com>
2024-09-28 23:57:16 -0300

    s

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>Página Não Encontrada</h1>
+      <p>O recurso que você está tentando acessar não existe.</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
